053b5e5dd9d9d9cf93b75d60a2cd311ae39f9647
johari2020 <42872004+johari2020@users.noreply.github.com>
2018-09-05 11:26:35 -0400

    Updated from Brython Server: 9/5/2018 11:26:33 AM

--- turtorial1.py
+++ turtorial1.py
@@ -1,3 +1,28 @@
 from ggame import App
 myapp = App()
 myapp.run()
+
+from ggame import App, Color, LineStyle, Sprite
+from ggame import RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset
+
+# Three priamary colors with not transparency (alpha = 1.0)
+red = Color(0xff0000, 1.0)
+green = Color(0x00ff00, 1.0)
+blue = Color(0x0000ff, 1.0)
+black = Color(0x000000, 1.0)
+ 
+
+# Define a line style that is a thin (1 pixel) wide black line
+thinline = LineStyle(2, black)
+
+rectangle = RectangleAsset(200, 80, thinline, blue) 
+
+ellipse = EllipseAsset(200, 80, thinline, red) 
+
+Sprite(rectangle, (200, 50)) 
+Sprite(rectangle, (100,25))
+
+Sprite(ellipse, (200,200))
+
+myapp = App()
+myapp.run()

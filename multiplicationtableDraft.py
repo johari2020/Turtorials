302cdab182c171f81231e845ab31eb6e04e4db20
johari2020 <42872004+johari2020@users.noreply.github.com>
2018-09-24 14:05:05 -0400

    Updated from Brython Server: 9/24/2018 2:05:03 PM

--- multiplicationtableDraft.py
+++ multiplicationtableDraft.py
@@ -1,1 +1,19 @@
+height = input("Width of multiplication table: ")
+width = input ("Height of multiplication table: ")
+h = int(height)
+w = int(width) 
+i=0
+a = range(i,h)
+b = range(i,w)
 
+l = list(range(1,w))
+m = list(range(1,h))
+
+t = l.*m
+print(t)
+
+    
+    
+for i in b:
+    i+=i-1
+    print(str(l)*(h))

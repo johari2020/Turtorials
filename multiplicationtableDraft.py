75f97751739c9840c129ea32adb782f80abeaf45
johari2020 <42872004+johari2020@users.noreply.github.com>
2018-09-27 11:53:57 -0400

    Updated from Brython Server: 9/27/2018 11:53:55 AM

--- multiplicationtableDraft.py
+++ multiplicationtableDraft.py
@@ -4,12 +4,13 @@ w=int(width)
 h=int(height)
 
 t = "X"
-    
-b=range(0,w)
+b=list(range(0,w)) 
+
 a=range(0,h)
 for j in a:
     j+=j-1
     for i in b:
-        i+=i-1
-        print(t, end = ' ' )
+        i+=1
+        c=list(range(1,i+1)) 
+        print((c), end = ' ' )
     print(" ")
